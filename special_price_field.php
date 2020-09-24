c1ddda21411c728517a9409f75c26eb6abae5c2a
bobimicroweber <bobi@microweber.com>
2020-09-24 14:39:45 +0300

    update

---
+++ special_price_field.php
@@ -0,0 +1,13 @@
+<?php must_have_access(); ?>
+<div class="form-group text-opacity-5">
+	<label>Special Price</label>
+	<div class="input-group mb-3 prepend-transparent append-transparent">
+		<div class="input-group-prepend">
+			<span class="input-group-text text-muted">BGN</span>
+		</div>
+		<input disabled=disabled type="text" class="form-control" name="special_price" value="">
+		<div class="input-group-append">
+			<span class="input-group-text" data-toggle="tooltip" title="" data-original-title="To put a product on sale, make Compare at price the original price and enter the lower amount into Price."><i class="mdi mdi-help-circle"></i></span>
+		</div>
+	</div>
+</div>
