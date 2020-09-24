03e017af9193af53a455e2919fd318667b065a64
bobimicroweber <bobi@microweber.com>
2020-09-24 16:00:14 +0300

    update

---
+++ src/ShopOffersServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bojidar
+ * Date: 9/24/2020
+ * Time: 3:38 PM
+ */
+namespace MicroweberPackages\Shop\Offers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ShopOffersServiceProvider extends ServiceProvider
+{
+   public function register(){
+
+
+    }
+
+}
